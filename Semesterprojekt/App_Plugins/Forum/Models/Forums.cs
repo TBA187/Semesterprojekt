d89205d82d2550093ee0c73d27256c0334e351c0
Sadri TBA <sadri_albaner@hotmail.com>
2017-12-13 17:14:28 +0100

    Forside fix

---
+++ Semesterprojekt/App_Plugins/Forum/Models/Forums.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Semesterprojekt.Models
+{
+    public class Forums
+    {
+        public int forumId { get; set; }
+
+        [Required]
+        public string ForumName { get; set; }
+
+        [Required]
+        public DateTime ForumDate { get; set; }
+
+        public int Fk_userid { get; set; }
+
+
+        //public class ForumsDBContext : DbContext
+        //{
+        //    public DbSet<Forums> Forums { get; set; }
+        //}
+    }
+}
